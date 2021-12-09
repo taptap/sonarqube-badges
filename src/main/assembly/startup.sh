4519ad0ca47f08fcd0e034bde7332af3e2cb5ab1
kl <chenkailing@xd.com>
2021-12-09 17:22:37 +0800

    update startup.sh

--- src/main/assembly/startup.sh
+++ src/main/assembly/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -Xmx1024M -Xms1024M -Dfile.encoding=UTF-8 -Dspring.config.location=./conf.properties -jar sonarqube-badges-0.1.jar > ./sonarqube-badges.log 2>&1 &
+nohup java -Xmx1024M -Xms1024M -Dfile.encoding=UTF-8 -Dspring.config.location=./conf.properties -jar sonarqube-badges-*.jar > ./sonarqube-badges.log 2>&1 &
